0faeabec30f33f7ca95d0051a867f4e2b8831169
gabrielc42 <clark.gabe97@gmail.com>
2022-10-28 20:30:28 -0500

    undeclared name error

--- cards/main.go
+++ cards/main.go
@@ -1,14 +1,23 @@
 package main
 
-import "fmt"
-
 // variables can be init outside of func, but not assigned values
 
 func main() {
 	// var card string = "Ace of Spades"
-	card := "Ace of Spades" // := only for new variables, or above (var variableName type)
+	// card := "Ace of Spades" // := only for new variables, or above (var variableName type)
 	// cannot do card = "Ace of Spades" (assigned value before init)
-	card = "Five of Diamonds" // reassigning
+	// card = "Five of Diamonds" // reassigning
 
-	fmt.Println(card)
+	// card := newCard()
+
+	// fmt.Println(card)
+
+	cards := deck{newCard(), newCard()}
+	cards = append(cards, newCard())
+
+	cards.print()
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
